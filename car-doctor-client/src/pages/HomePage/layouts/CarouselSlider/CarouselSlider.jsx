56b6f27c4324784aaee4db63b5943bd83fe7876e
Developer Barry <barryallen2558@gmail.com>
2024-11-30 21:37:22 +0600

    Customize position of next/prev buttons

--- car-doctor-client/src/pages/HomePage/layouts/CarouselSlider/CarouselSlider.jsx
+++ car-doctor-client/src/pages/HomePage/layouts/CarouselSlider/CarouselSlider.jsx
@@ -42,9 +42,9 @@ const CarouselSlider = () => {
     return (
         <div className="container mx-auto px-4 md:px-12 lg:px-24 py-4">
             <div className="relative w-full">
-                
+
                 {/* Carousel wrapper */}
-                <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+                <div className="relative h-56 overflow-hidden rounded-lg md:h-[550px]">
                     {images.map((image, index) => (
                         <div
                             key={index}
@@ -61,54 +61,56 @@ const CarouselSlider = () => {
                 </div>
 
                 {/* Slider controls */}
-                <button
-                    type="button"
-                    onClick={() => handlePrev(currentIndex)}
-                    className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-                >
-                    <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                        <svg
-                            className="w-4 h-4 text-white dark:text-gray-800"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 6 10"
-                        >
-                            <path
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M5 1 1 5l4 4"
-                            />
-                        </svg>
-                        <span className="sr-only">Previous</span>
-                    </span>
-                </button>
-                <button
-                    type="button"
-                    onClick={() => handleNext(currentIndex)}
-                    className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-                >
-                    <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                        <svg
-                            className="w-4 h-4 text-white dark:text-gray-800"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 6 10"
-                        >
-                            <path
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="m1 9 4-4-4-4"
-                            />
-                        </svg>
-                        <span className="sr-only">Next</span>
-                    </span>
-                </button>
+                <div className="absolute flex bottom-10 right-8">
+                    <button
+                        type="button"
+                        onClick={() => handlePrev(currentIndex)}
+                        className=" bottom-0 left-0 z-30 flex items-center h-full px-3 cursor-pointer group focus:outline-none"
+                    >
+                        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-[#FFFFFF33] dark:bg-[#FFFFFF33] group-hover:bg-[#FFFFFF33] dark:group-hover:bg-[#e1d2d233] group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                            <svg
+                                className="w-4 h-4 text-white dark:text-gray-800"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 6 10"
+                            >
+                                <path
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M5 1 1 5l4 4"
+                                />
+                            </svg>
+                            <span className="sr-only">Previous</span>
+                        </span>
+                    </button>
+                    <button
+                        type="button"
+                        onClick={() => handleNext(currentIndex)}
+                        className=" top-0 right-0 z-30 flex items-center h-full px-3 cursor-pointer group focus:outline-none"
+                    >
+                        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-[#FF3811] dark:bg-[#FF3811] group-hover:bg-[#b3260a] dark:group-hover:bg-[#b3260a] group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-[#FFFFFF33] group-focus:outline-none">
+                            <svg
+                                className="w-4 h-4 text-white dark:text-gray-800"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 6 10"
+                            >
+                                <path
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="m1 9 4-4-4-4"
+                                />
+                            </svg>
+                            <span className="sr-only">Next</span>
+                        </span>
+                    </button>
+                </div>
             </div>
         </div>
     );
