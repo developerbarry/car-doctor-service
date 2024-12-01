5494a9955b1872c4b7e4c2bfefacee9a8a089ef5
Developer Barry <barryallen2558@gmail.com>
2024-12-02 02:09:59 +0600

    added user observer

--- car-doctor-client/src/AuthProvider/AuthProvider.jsx
+++ car-doctor-client/src/AuthProvider/AuthProvider.jsx
@@ -1,24 +1,44 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import auth from "../firebase/firebase.config";
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
 
     const createUser = (email, password) => {
+        setIsLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
     const userSignIn = (email, password) => {
+        setIsLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
     }
 
+
+    useEffect(() => {
+        const unSubscrbe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            setIsLoading(false)
+        })
+
+        return () => {
+            unSubscrbe()
+        }
+    }, [])
+
     const authInfo = {
         createUser,
-        userSignIn
+        userSignIn,
+        user,
+        isLoading
     }
+
+    console.log(user)
 
     return (
         <AuthContext.Provider value={authInfo}>
